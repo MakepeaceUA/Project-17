2edec108c3514f0ced81470286ea0f1a1315d3e6
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-09-07 20:06:51 +0300

    Add files via upload

---
+++ ДЗ.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+// Задание №2
+//void Func(int list[],int S) 
+//{
+//  double sum = 0;
+//  for (int i = 0;i <= S;i++) 
+//  {
+//    sum += list[i];
+//  }
+//  cout << "Сумма элементов списка: " << sum << "\n";
+//  cout << "Среднее арифметическое элементов списка: " << sum / S;
+//}
+
+//int main()
+//{
+//  setlocale(0, "RUS");
+
+//  const int size = 10;
+//  int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//  Func(arr,size);
+//}
+
+
+//Задание №3
+//void Func(int list[],int S) 
+//{
+//  int Plus = 0;
+//  int Minus = 0;
+//  int Null = 0;
+//  for (int i = 0;i < S;i++) 
+//  {
+//    if (list[i] > 0) 
+//    {
+//      Plus++;
+//    }
+//    else if (list[i] < 0) 
+//    {
+//      Minus++;
+//    }
+//    else if (list[i] == 0) 
+//    {
+//      Null++;
+//    }
+//  }
+//  cout << "Количество положительных элементов списка: " << Plus << "\n";
+//  cout << "Количество отрицательных элементов списка: " << Minus << "\n";
+//  cout << "Количество нулевых элементов списка: " << Null << "\n";
+//}
+
+//int main()
+//{
+//  setlocale(0, "RUS");
+
+//  const int size = 10;
+//  int arr[] = {1,2,0,4,-5,6,7,-8,9,10};
+//  Func(arr,size);
+//}
